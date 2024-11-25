641955b782444e978c54f8633e753174585cbe65
TonyGrif <TonyGriffin2000@gmail.com>
2024-11-25 18:31:08 -0500

    treesitter: add python to installed

--- lua/plugins/code/treesitter.lua
+++ lua/plugins/code/treesitter.lua
@@ -4,7 +4,7 @@ return {
     build = ":TSUpdate",
     config = function ()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = { "lua", "nix", "vim", "vimdoc" },
+        ensure_installed = { "lua", "nix", "python", "vim", "vimdoc" },
         sync_install = false,
         highlight = { enable = true },
         indent = { enable = true },
