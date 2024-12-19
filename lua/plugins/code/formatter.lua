f0b22c611bdace74f3f45224c8613968228e533c
TonyGrif <TonyGriffin2000@gmail.com>
2024-12-19 14:20:18 -0500

    fix(format): error with false checking

--- lua/plugins/code/formatter.lua
+++ lua/plugins/code/formatter.lua
@@ -18,7 +18,7 @@ return {
     require("conform").setup({
       format_on_save = function(bufnr)
         -- Disable with a global or buffer-local variable
-        if not vim.g.autoformat or not vim.b[bufnr].autoformat then
+        if vim.g.autoformat == false or vim.b[bufnr].autoformat == false then
           return
         end
         return { timeout_ms = 500, lsp_format = "fallback" }
