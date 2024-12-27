4c6f1e405fa643041d7b2176dd7dc086f21cb952
TonyGrif <TonyGriffin2000@gmail.com>
2024-12-27 14:38:27 -0500

    core(autocmd): add easy quit for quickfix menu

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,7 +1,7 @@
 -- [[ LSP Mappings ]]
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(event)
-    local opts = {buffer = event.buf}
+    local opts = { buffer = event.buf }
 
     vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
     vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
@@ -11,7 +11,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
     vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
     vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-    vim.keymap.set({"n", "x"}, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+    vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
     vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
   end,
 })
@@ -26,7 +26,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- [[ Lua ]]
-vim.api.nvim_create_autocmd({"VimEnter", "BufReadPre", "FileType"}, {
+vim.api.nvim_create_autocmd({ "VimEnter", "BufReadPre", "FileType" }, {
   desc = "Set settings for lua files",
   group = vim.api.nvim_create_augroup("lang-lua", { clear = true }),
   pattern = "*.lua",
@@ -37,12 +37,35 @@ vim.api.nvim_create_autocmd({"VimEnter", "BufReadPre", "FileType"}, {
 })
 
 -- [[ Nix ]]
-vim.api.nvim_create_autocmd({"VimEnter", "BufReadPre", "FileType"}, {
+vim.api.nvim_create_autocmd({ "VimEnter", "BufReadPre", "FileType" }, {
   desc = "Set settings for nix files",
   group = vim.api.nvim_create_augroup("lang-nix", { clear = true }),
   pattern = "*.nix",
   callback = function()
     vim.opt.shiftwidth = 2
     vim.opt.tabstop = 2
+  end,
+})
+
+-- [[ Close with q ]]
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  desc = "Close filetype with 'q'",
+  pattern = {
+    "qf",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.schedule(
+      function()
+        vim.keymap.set("n", "q",
+          function()
+            vim.cmd("close")
+            pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+          end, {
+            buffer = event.buf,
+            silent = true,
+          })
+      end
+    )
   end,
 })
