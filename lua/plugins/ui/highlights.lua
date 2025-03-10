71cc27b74d89bcc7f64289b791ccea80b95a7f62
TonyGrif <TonyGriffin2000@gmail.com>
2025-03-10 14:23:33 -0400

    plugin(ui): simplify todo-comments plugin

--- lua/plugins/ui/highlights.lua
+++ lua/plugins/ui/highlights.lua
@@ -3,15 +3,7 @@ return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
-      keywords = {
-        FIX = { icon = vim.g.have_nerd_font and " " or " " },
-        TODO = { icon = vim.g.have_nerd_font and " " or " " },
-        HACK = { icon = vim.g.have_nerd_font and " " or " " },
-        WARN = { icon = vim.g.have_nerd_font and " " or " " },
-        PERF = { icon = vim.g.have_nerd_font and " " or " " },
-        NOTE = { icon = vim.g.have_nerd_font and " " or " " },
-        TEST = { icon = vim.g.have_nerd_font and "⏲ " or " " },
-      },
+      signs = vim.g.have_nerd_font,
     },
   },
 }
