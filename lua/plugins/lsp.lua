4acad0255062b344a743eaaab21f3b11afa10e11
TonyGrif <TonyGriffin2000@gmail.com>
2025-07-31 23:38:31 -0400

    plugins(lsp): add lspconfig and configure it to ignore noninstalled servers

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,30 @@
+return {
+  "neovim/nvim-lspconfig",
+  config = function()
+    local lspconfig = require("lspconfig")
+    local servers = {
+      lua_ls = {
+        cmd = "lua-language-server",
+        config = {
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim" }
+              }
+            }
+          }
+        }
+      },
+      pyright = {
+        cmd = "pyright",
+        config = {},
+      },
+    }
+
+    for server, data in pairs(servers) do
+      if vim.fn.executable(data.cmd) == 1 then
+        lspconfig[server].setup(data.config)
+      end
+    end
+  end,
+}
