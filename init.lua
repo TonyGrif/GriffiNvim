07815e8cddf4f6d42d9efbd0f94a2e2c38e9028f
TonyGrif <TonyGriffin2000@gmail.com>
2025-07-31 23:32:29 -0400

    core(mappings): add basic key remaps

--- init.lua
+++ init.lua
@@ -1,6 +1,11 @@
 --[
 -- Yet Another Neovim Configuration
+--
+-- Requirements:
+--  * Neovim >= 0.8.0 (lazy.vim)
+--  * Git >= 2.19.0 (lazy.vim)
 --]
 
 require("core.globals")
 require("core.options")
+require("core.mappings")
