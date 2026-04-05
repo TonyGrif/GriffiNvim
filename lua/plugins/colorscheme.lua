ae651f4b1e31e78fb7c03f5c9e69056f485a3f17
TonyGrif <TonyGriffin2000@gmail.com>
2026-04-04 21:02:48 -0400

    lua(mod): modularize code

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+  'folke/tokyonight.nvim',
+  priority = 1000,
+  config = function()
+    ---@diagnostic disable-next-line: missing-fields
+    require('tokyonight').setup {
+      styles = {
+        comments = { italic = false },
+      },
+    }
+    vim.cmd.colorscheme 'tokyonight-night'
+  end,
+}
